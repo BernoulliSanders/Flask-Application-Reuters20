e1c4368d05b2ff2beea4c79364e6cf92513f9925
BernoulliSanders <samlogan86@gmail.com>
2016-10-07 22:32:03 +0100

    Working version with 20 Newsgroups

--- Reuters_read_in.py
+++ Reuters_read_in.py
@@ -1,37 +1,31 @@
 import numpy as np
 import pandas as pd
 from sklearn.datasets import fetch_rcv1, fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 import sqlite3
 
-'''
-rcv1 = fetch_rcv1()
 
-#pd.read_pickle('lyrl2004_tokens_train.dat')
-
-
-f = open('lyrl2004_tokens_train.dat', 'rb')  # We need to re-open the file
-data = f.read()
-train = np.fromfile(data,sep="")
-f.close()'''
-
-# 20 newsgroups
+# 20 newsgroups - Train
 politics = ['talk.politics.mideast', 'talk.politics.guns']
 newsgroups_train = fetch_20newsgroups(subset='train',remove=('headers', 'footers', 'quotes'), categories=politics)
 vectorizer = TfidfVectorizer()
-vectors = vectorizer.fit_transform(newsgroups_train.data)
-clf = LogisticRegression()
-clf.fit(vectors, newsgroups_train.target)
-newsgroups_test = fetch_20newsgroups(subset='test',remove=('headers', 'footers', 'quotes'), categories=politics)
 
+# Take just 10 examples as active learning
+vectors = vectorizer.fit_transform(newsgroups_train.data)[0:20]
+clf = LogisticRegression(C=10)
+clf.fit(vectors, newsgroups_train.target[0:20])
+
+# Test Set
+newsgroups_test = fetch_20newsgroups(subset='test',remove=('headers', 'footers', 'quotes'), categories=politics)
 vectors_test = vectorizer.transform(newsgroups_test.data)
-pred = clf.predict(vectors_test)
-metrics.f1_score(newsgroups_test.target, pred, average='binary')
+metrics.f1_score(newsgroups_test.target, clf.predict(vectors_test), average='binary')
 test_set = pd.DataFrame(newsgroups_test.data)
-test_set['class_proba'] = clf.predict_proba(vectors_test)[:,0]
-test_set.columns = ['text', 'class_proba']
+test_set['class_proba'] = clf.predict_proba(vectors_test)[:,1] # Probability of being in class 1, which is the second column returned by .predict_proba
+test_set['prediction'] = clf.predict(vectors_test)
+test_set.columns = ['text', 'class_proba', 'prediction']
+test_set['predicted_labels'] = test_set['prediction'].map({0:'Middle-east', 1:'Guns'})
 
 conn = sqlite3.connect('reuters_testX_with_proba.sqlite')
 c = conn.cursor()
--- app.py
+++ app.py
@@ -10,13 +10,17 @@ from vectorizer import vect
 
 app = Flask(__name__)
 
-######## Preparing the Classifier
+
 cur_dir = os.path.dirname(__file__)
 clf = pickle.load(open(os.path.join(cur_dir,
                  'pkl_objects',
-                 'classifier.pkl'), 'rb'))
+                 'Reuters20-classifier.pkl'), 'rb'))
 db = os.path.join(cur_dir, 'reviews.sqlite')
 db2 = os.path.join(cur_dir, 'reuters_testX_with_proba.sqlite')
+
+
+ordered_weights_dict = pickle.load(open(os.path.join(cur_dir,'pkl_objects','ordered_weights_dict.pkl'), 'rb'))
+
 def classify(document):
     label = {0: 'negative', 1: 'positive'}
     X = vect.transform([document])
@@ -36,16 +40,65 @@ def sqlite_entry(path, document, y):
     conn.commit()
     conn.close()
 
-######## Flask
-class ReviewForm(Form):
-    moviereview = TextAreaField('',
-                                [validators.DataRequired(),
-                                validators.length(min=15)])
+def train_model(document, y):
+    X = vect.transform([document])
+    clf.partial_fit(X, [y])
 
+def update_class_proba(path):
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+    cursor = c.execute('SELECT text, indexID FROM reuters_test_X')
+    all_rows = cursor.fetchall()
+    X = vect.transform(x[0] for x in all_rows)
+    new_proba = list(float(z) for z in clf.predict_proba(X)[:, 1])
+    IDs = list(int(zz) for zz in np.arange(0, 740, 1))
+    new_proba_tuple = list(zip(new_proba,IDs))
+    #c.execute('ALTER TABLE reuters_test_X ADD COLUMN predict_proba_tplus1 REAL')
+    #c.executemany('UPDATE reuters_test_X SET predict_proba_tplus1=? WHERE indexID=?', new_proba_tuple)
+    c.executemany('UPDATE reuters_test_X SET class_proba=? WHERE indexID=?', new_proba_tuple)
+    new_class = list(int(xy) for xy in clf.predict(X))
+    new_class_tuple = list(zip(new_class,IDs))
+    c.executemany('UPDATE reuters_test_X SET prediction=? WHERE indexID=?', new_class_tuple)
+    c.execute('UPDATE reuters_test_X SET predicted_labels=\'Guns\' WHERE prediction=1')
+    c.execute('UPDATE reuters_test_X SET predicted_labels=\'Middle-east\' WHERE prediction=0')
+    conn.commit()
+    conn.close()
+
+# This gives the index location of the weight
+def look_up_weight(word):
+    return list(ordered_weights_dict.keys()).index(word)
+
+# This takes the index location of the weight and updates it
+def increase_weight(index):
+    if clf.coef_[0][index] !=0:
+        clf.coef_[0][index] = clf.coef_[0][index] * 10
+    else:
+        clf.coef_[0][index] = 1
+
+def decrease_weight(index):
+    clf.coef_[0][index] = clf.coef_[0][index] / 10
+
+def uncertainty_sample(class_proba):
+    uncertainty = abs(class_proba - 0.5)
+    return uncertainty
+
+
+### Forms
+# Need to add a constraint to only allow single words? Or if not then to split them myself and vectorize
+class ReviewForm(Form):
+    feature_feedback = TextAreaField('',
+                                [validators.DataRequired(),
+                                validators.length(min=3)])
+
+
+### Flask functions
+
+'''
 @app.route('/')
 def index():
     form = ReviewForm(request.form)
     return render_template('reviewform.html', form=form)
+'''
 
 @app.route('/results', methods=['POST'])
 def results():
@@ -59,12 +112,12 @@ def results():
                                 probability=round(proba*100, 2))
     return render_template('reviewform.html', form=form)
 
+
 @app.route('/thanks', methods=['POST'])
 def feedback():
     feedback = request.form['feedback_button']
     review = request.form['review']
     prediction = request.form['prediction']
-
     inv_label = {'negative': 0, 'positive': 1}
     y = inv_label[prediction]
     if feedback == 'Incorrect':
@@ -73,17 +126,51 @@ def feedback():
     sqlite_entry(db, review, y)
     return render_template('thanks.html')
 
-@app.route('/article/<sentiment>')
-def display_article(sentiment):
+@app.route('/update', methods=['POST'])
+def update_classifier_feedback():
+    feedback = request.form['update_classifier']
+    article = request.form['uncertain_article']
+    prediction = request.form['prediction']
+    inv_label = {"Middle-east": 0, "Guns": 1}
+    y = inv_label[prediction]
+    if feedback == 'Incorrect':
+        y = int(not(y))
+    # Retrain model with uncertain article and new (or same as before) label
+    train_model(article, y)
+    # Update class probabilities and labels for entire test set
+    update_class_proba(db2)
+    return render_template('thanks.html')
+
+@app.route('/change-weights', methods=['POST'])
+def manually_change_weights():
+    feedback = request.form['feature_feedback']
+    increase_weight(look_up_weight(feedback))
+    return render_template('thank-you.html')
+
+
+# thanks.html leads here
+@app.route('/article')
+def display_article():
     conn = sqlite3.connect(db2)
+    conn.create_function("uncertainty_query",1,uncertainty_sample)
     c = conn.cursor()
-    if sentiment == '1':
-        cursor = c.execute('SELECT text, class_proba FROM reuters_test_X WHERE class_proba >= 0.5')
-    else:
-        cursor = c.execute('SELECT text, class_proba FROM reuters_test_X WHERE class_proba < 0.5')
-    #class_proba = c.execute('SELECT class_proba FROM mini_testX')
-    #sorted_proba = np.argsort(class_proba)
-    return render_template('article.html', items=cursor.fetchone())
+    cursor = c.execute('SELECT predicted_labels, text, MIN(uncertainty_query(class_proba)) FROM reuters_test_X')
+    items = [dict(predicted_labels=row[0], text=row[1], class_proba=row[2]) for row in cursor.fetchall()]
+    #items = {predicted_labels:cursor[0], text:cursor[1], class_proba:cursor[2]}
+    return render_template('article.html', items=items)
+
+
+# Need a second version of thanks.html to loop back here
+@app.route('/article-with-reweighting')
+def display_article_manual_reweighting():
+    conn = sqlite3.connect(db2)
+    conn.create_function("uncertainty_query", 1, uncertainty_sample)
+    c = conn.cursor()
+    cursor = c.execute('SELECT predicted_labels, text, MIN(uncertainty_query(class_proba)) FROM reuters_test_X')
+    items = [dict(predicted_labels=row[0], text=row[1], class_proba=row[2]) for row in cursor.fetchall()]
+    form = ReviewForm(request.form)
+    return render_template('article-with-reweighting.html', items=items, form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
